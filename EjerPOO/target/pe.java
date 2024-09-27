af12c023320cc55463edf8439684ab1c3d5f6e16
Luissssssssssssssss <barriosluis543890@gmail.com>
2024-09-26 18:46:42 -0600

    Add files via upload

---
+++ EjerPOO/target/pe.java
@@ -0,0 +1,29 @@
+public abstract class ProductoElectronico {
+    private double precio;
+    private String marca;
+    private String modelo;
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+}
